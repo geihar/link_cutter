6db0cd11ff913a74c1788b8fee1159f430225b32
geihar <l0635968488@gmail.com>
2020-09-21 14:10:38 +0200

    add popup to copy to clipboard

--- static/index.js
+++ static/index.js
@@ -3,6 +3,14 @@ function copy(e) {
     let copyText = document.querySelector("#link");
     copyText.select();
     document.execCommand("copy");
+    document.querySelector("#copy").style.display='none';
+    copyText.value = '';
+    var dialog = document.querySelector('dialog');
+    dialog.show();
+    setTimeout(function(){ dialog.close(); }, 2500);
+
+
 }
 
 document.querySelector("#copy").addEventListener("click", copy);
+
